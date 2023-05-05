399a75d7094e6959e76b5aca4f982b617697a019
azzy9 <19675382+azzy9@users.noreply.github.com>
2023-05-05 18:55:46 +0100

    Clean up code

--- addon.py
+++ addon.py
@@ -1,8 +1,19 @@
-import re, json, sys, requests
-import xbmc, xbmcgui, xbmcaddon, xbmcplugin
+import ssl
+
+import xbmc
+import xbmcgui
+import xbmcaddon
+import xbmcplugin
+
+import sys
+import re
+
+import json
+import requests
+
 import datetime
 from time import time, sleep
-import ssl
+
 import six
 from six.moves import urllib, urllib_parse
 
@@ -59,8 +70,11 @@ def setRawWindowProperty(prop, data):
     window = xbmcgui.Window(xbmcgui.getCurrentWindowId())
     window.setProperty(prop, data)
 
-def fetchURL(url, data=None, extraHeaders=None):
-    myHeaders = {
+def fetch_url(url, data=None, extra_headers=None):
+
+    """ fetches data from URL """
+
+    my_headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml,application/json;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
@@ -68,46 +82,57 @@ def fetchURL(url, data=None, extraHeaders=None):
         'Pragma': 'no-cache',
         'DNT': '1'
     }
-    if extraHeaders:
-        myHeaders.update(extraHeaders)
 
-    # At the moment it's a single response cookie, "__cfduid". Other cookies are set w/ Javascript by ads.
-    cookieProperty = getRawWindowProperty(PROPERTY_SESSION_COOKIE)
-    if cookieProperty:
-        cookieDict = dict(pair.split('=') for pair in cookieProperty.split('; '))
+    if extra_headers:
+        my_headers.update(extra_headers)
+
+    cookie_property = getRawWindowProperty(PROPERTY_SESSION_COOKIE)
+    if cookie_property:
+        cookie_dict = dict(pair.split('=') for pair in cookie_property.split('; '))
     else:
-        cookieDict = None
+        cookie_dict = None
 
-    startTime = time()
+    start_time = time()
 
     status = 0
     i = -1
     while status != 200 and i < 2:
         if data:
-            response = s.post(url, data=data, headers=myHeaders, verify=False, cookies=cookieDict, timeout=10)
+            response = s.post(
+                url, data=data, headers=my_headers, verify=False, cookies=cookie_dict, timeout=10
+            )
         else:
-            response = s.get(url, headers=myHeaders, verify=False, cookies=cookieDict, timeout=10)
+            response = s.get(
+                url, headers=my_headers, verify=False, cookies=cookie_dict, timeout=10
+            )
         status = response.status_code
         i += 1
         if status == 403 and 'cloudflare' in response.headers.get('Expect-CT', ''):
-            s.mount(BASEURL, tls_adapters[i])
+            s.mount(WEB_URL, tls_adapters[i])
 
     # Store the session cookie(s), if any.
-    if not cookieProperty and response.cookies:
+    if not cookie_property and response.cookies:
         setRawWindowProperty(
-            PROPERTY_SESSION_COOKIE, '; '.join(pair[0]+'='+pair[1] for pair in response.cookies.get_dict().items())
+            PROPERTY_SESSION_COOKIE, '; ' . \
+            join(pair[0]+'='+pair[1] for pair in response.cookies.get_dict().items())
         )
 
-    elapsed = time() - startTime
+    elapsed = time() - start_time
     if elapsed < 0.5:
         sleep(0.5 - elapsed)
 
     return response
 
 def construct_request(query):
+
+    """ Constructs Query """
+
     return base_url + "?" + urllib_parse.urlencode(query)
 
 def keyboard_input(heading='', message=''):
+
+    """ Requests for keyboard input """
+
     search_string = None
     keyboard = xbmc.Keyboard(message, heading)
     keyboard.doModal()
@@ -115,17 +140,18 @@ def keyboard_input(heading='', message=''):
         search_string = keyboard.getText()
     return search_string
 
-# extract appropriate data from API call 
 def video_meta( data, return_data, variant='' ):
+
+    """ extract appropriate data from API call """
 
     try:
         return_data['genre'] = data['extensions']['genre']
-    except:
+    except Exception:
         pass
 
     try:
         return_data['country'] = data['extensions']['country']
-    except:
+    except Exception:
         pass
 
     if variant == 'getLiveChannels':
@@ -133,7 +159,7 @@ def video_meta( data, return_data, variant='' ):
     else:
         try:
             return_data['year'] = data['extensions']['year']
-        except:
+        except Exception:
             pass
 
     if 'setid' not in return_data.keys() and 'id' in data.keys():
@@ -141,22 +167,22 @@ def video_meta( data, return_data, variant='' ):
 
     try:
         return_data['rating'] = data['extensions']['imdb_score'].replace('Imdb Rating:','').strip()
-    except:
+    except Exception:
         pass
 
     try:
         return_data['director'] = data['extensions']['director']
-    except:
+    except Exception:
         pass
 
     try:
         return_data['plot'] = data['extensions']['description']
-    except:
+    except Exception:
         pass
 
     try:
         return_data['plotoutline'] = data['summary']
-    except:
+    except Exception:
         pass
 
     if 'title' not in return_data.keys() and 'title' in data.keys():
@@ -164,12 +190,12 @@ def video_meta( data, return_data, variant='' ):
 
     try:
         return_data['duration'] = data['extensions']['duration']
-    except:
+    except Exception:
         pass
 
     try:
         return_data['mediatype'] = data['type']['value']
-    except:
+    except Exception:
         pass
 
     return return_data
@@ -190,7 +216,7 @@ if mode == '':
         list_item.setArt({
             'icon':MEDIA_URL + media_names[i] + '.jpg',
             'poster':MEDIA_URL + media_names[i] + '.jpg',
-        });
+        })
         callback = construct_request({
             'mode': 'list',
             'type': variant,
@@ -218,15 +244,15 @@ elif mode == 'list':
     if variant == 'movies':
         extra_params = '?limit=9999'
 
-    list = fetchURL( WEB_URL + '/' + variant + extra_params ).json()
+    list_data = fetch_url( WEB_URL + '/' + variant + extra_params ).json()
 
-    for item in list[ 'entry' ]:
+    for item in list_data[ 'entry' ]:
 
         if is_search:
             variant = media_types[ search_types.index(item['type']['value']) ]
             xbmc.log( variant, xbmc.LOGWARNING )
 
-        title = item['title'];
+        title = item['title']
         list_item = xbmcgui.ListItem( title )
         mode = media_mode[ media_types.index(variant) ]
 
@@ -268,9 +294,9 @@ elif mode == 'list':
 
             list_item.setInfo( 'Video', infoLabels )
             list_item.setProperty('IsPlayable', 'true')
-            isFolder = False
+            is_folder = False
         else:
-            isFolder = True
+            is_folder = True
 
         callback = construct_request({
             'id': item['id'],
@@ -284,30 +310,30 @@ elif mode == 'list':
             handle = addon_handle,
             url = callback,
             listitem = list_item,
-            isFolder = isFolder
+            isFolder = is_folder
         )
 
     xbmcplugin.endOfDirectory(addon_handle)
 
 elif mode == 'episodes':
 
-    seasons = fetchURL( WEB_URL + '/' + 'getSeasonTabs?series_id=' + id ).json()
+    seasons = fetch_url( WEB_URL + '/' + 'getSeasonTabs?series_id=' + id ).json()
 
     for season in seasons[ 'entry' ]:
 
         try:
             season_number = season['title'].replace('Season','').strip()
-        except:
+        except Exception:
             season_number = '1'
 
-        episodes = fetchURL( WEB_URL + '/' + 'getSeason?season_id=' + season['id'] ).json()
+        episodes = fetch_url( WEB_URL + '/' + 'getSeason?season_id=' + season['id'] ).json()
 
         # year is set per season
         year = ''
 
         for episode in episodes[ 'entry' ]:
 
-            episode_title = episode['title'];
+            episode_title = episode['title']
             list_item = xbmcgui.ListItem( episode_title )
             mode = media_mode[ media_types.index(variant) ]
 
@@ -371,12 +397,14 @@ elif mode == 'play':
     if isdirect:
         video = args.get("url", [""])[0]
     else:
-       video = fetchURL( WEB_URL + '/' + play_endpoint[ media_types.index(variant) ] + '?id=' + id ).json()
+        video = fetch_url(
+            WEB_URL + '/' + play_endpoint[ media_types.index(variant) ] + '?id=' + id
+        ).json()
 
     if video:
-    
+
         infoLabels={ 'setid': id }
-    
+
         if isdirect:
             source = video
             infoLabels['title'] = title
